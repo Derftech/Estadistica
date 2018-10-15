dc2455b9f34c3cbef8b74dde62c1e70440e3d23f
Diego Rdz <derf_rvd@hotmail.com>
2018-10-15 12:19:10 -0500

    Inicio

---
+++ Estadistica/Estadistica/Ests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estadistica
+{
+    class Ests
+    {
+        public int a;
+        public int b;
+
+        public Ests(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int suma()
+        {
+            return a + b;
+        }
+        public int resta()
+        {
+            return a - b;
+        }
+        public int multiplicacion()
+        {
+            return a * b;
+        }
+        public int division()
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Estadistica/Estadistica/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estadistica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Operaciones básicas");
+            Ests a = new Ests(0, 0);
+
+            try
+            {
+                Console.WriteLine("Ingrese a: ");
+                a.a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese b: ");
+                a.b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Suma: " + a.suma());
+                Console.WriteLine("Resta: " + a.resta());
+                Console.WriteLine("Multiplicacíón: " + a.multiplicacion());
+                Console.WriteLine("División: " + a.division());
+
+            }
+
+            catch
+            {
+                Console.WriteLine("Debes ingresar números");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
